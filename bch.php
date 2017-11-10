1a923a120c82edb892d5b83c42e9689c6a9b9ca8
Maryamuzzamani <me.ipoelhuda@gmail.com>
2017-11-10 18:53:24 +0700

    ganti extensi jadi php

--- bch.php
+++ bch.php
@@ -380,7 +380,7 @@ function convertToRupiah(angka)
   var rupiah = '';    
   var angkarev = angka.toString().split('').reverse().join('');
   for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
-  return rupiah.split('Rp. ',rupiah.length-1).reverse().join('');
+  return 'Rp. '+rupiah.split('',rupiah.length-1).reverse().join('');
 }
 
 $(document).ready(function () {
